247574d468e77afa5f8b53940c0c685272ed650c
mouradeljayi <mouradeljayi98@gmail.com>
2022-05-16 15:07:19 +0100

    create flickr api config

--- src/main/java/com/elj/gestioncinema/services/strategy/SaveUserPhoto.java
+++ src/main/java/com/elj/gestioncinema/services/strategy/SaveUserPhoto.java
@@ -1,6 +1,6 @@
 package com.elj.gestioncinema.services.strategy;
 
-import com.elj.gestioncinema.dto.UserResponseDto;
+import com.elj.gestioncinema.dto.UserDto;
 import com.elj.gestioncinema.services.FlickrService;
 import com.elj.gestioncinema.services.UserService;
 import com.flickr4java.flickr.FlickrException;
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.io.InputStream;
 
 @Service("userStrategy")
-public class SaveUserPhoto implements Strategy<UserResponseDto> {
+public class SaveUserPhoto implements Strategy<UserDto> {
 
     private final FlickrService flickrService;
     private final UserService userService;
@@ -20,8 +20,8 @@ public class SaveUserPhoto implements Strategy<UserResponseDto> {
     }
 
     @Override
-    public UserResponseDto savePhoto(Long id, InputStream photo, String titre) throws FlickrException {
-        UserResponseDto user = userService.findById(id);
+    public UserDto savePhoto(Long id, InputStream photo, String titre) throws FlickrException {
+        UserDto user = userService.findById(id);
         String urlPhoto = flickrService.savePhoto(photo, titre);
         user.setPhoto(urlPhoto);
         return userService.save(user);
